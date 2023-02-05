884d4686b970357543846a7b5437fecd0700d332
Vincent <lvince95@gmail.com>
2023-02-06 06:30:41 +0800

    trpc setup

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,42 @@
+/**
+ * This file contains tRPC's HTTP response handler
+ */
+import * as trpcNext from '@trpc/server/adapters/next';
+import { createContext } from '@/server/context';
+import { appRouter } from '@/server/routers/_app';
+
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  /**
+   * @link https://trpc.io/docs/context
+   */
+  createContext,
+  /**
+   * @link https://trpc.io/docs/error-handling
+   */
+  onError:
+    process.env.NODE_ENV === 'development'
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`,
+          );
+        }
+      : ({ error }) => {
+          if (error.code === 'INTERNAL_SERVER_ERROR') {
+            // send to bug reporting
+            console.error('Something went wrong', error);
+          }
+        },
+  /**
+   * Enable query batching
+   */
+  batching: {
+    enabled: true,
+  },
+  /**
+   * @link https://trpc.io/docs/caching#api-response-caching
+   */
+  // responseMeta() {
+  //   // ...
+  // },
+});
