785270621b311b592a02d0579c8c4fd537ff2164
Vincent <lvince95@gmail.com>
2023-02-06 06:41:34 +0800

    add cypress integration/e2e tests

---
+++ cypress/e2e/calculator.cy.ts
@@ -0,0 +1,55 @@
+const testData = [
+  { input: '1 + 1', expectedResult: '2', numberOfOperations: 1 },
+  { input: '2 * 2', expectedResult: '4', numberOfOperations: 1 },
+  { input: '1 + 2 + 3', expectedResult: '6', numberOfOperations: 2 },
+  { input: '6 / 2', expectedResult: '3', numberOfOperations: 1 },
+  { input: '11 + 23', expectedResult: '34', numberOfOperations: 1 },
+  { input: '11.1 + 23', expectedResult: '34.1', numberOfOperations: 2 },
+  { input: '1 + 1 * 3', expectedResult: '4', numberOfOperations: 2 },
+  {
+    input: '( 11.5 + 15.4 ) + 10.1',
+    expectedResult: '37',
+    numberOfOperations: 2,
+  },
+  {
+    input: '23 - ( 29.3 - 12.5 )',
+    expectedResult: '6.2',
+    numberOfOperations: 2,
+  },
+  { input: '( 1 / 2 ) - 1 + 1', expectedResult: '0.5', numberOfOperations: 3 },
+  {
+    input: '10 - ( 2 + 3 * ( 7 - 5 ) )',
+    expectedResult: '2',
+    numberOfOperations: 4,
+  },
+];
+
+describe('e2e', () => {
+  it('should correctly calculate the given arithmetic expressions', () => {
+    cy.visit('/calculator');
+
+    for (const data of testData) {
+      cy.findByRole('textbox', {
+        name: /arithmetic expression/i,
+      }).type(data.input);
+
+      cy.findByRole('button', {
+        name: /compute/i,
+      }).click();
+
+      cy.findByText(`Result: ${data.expectedResult}`).should('exist');
+
+      cy.findByText(`performed`).should('exist');
+
+      cy.wait(1000);
+
+      cy.findByRole('button', {
+        name: /clear/i,
+      }).click();
+
+      cy.wait(500);
+    }
+  });
+});
+
+export {};
---
+++ cypress/e2e/deposit.cy.ts
@@ -0,0 +1,112 @@
+const depositPlans = [
+  {
+    id: 0,
+    portfolio: 'High risk',
+    amount: 10000,
+    depositFrequency: 'One-Time',
+  },
+  {
+    id: 1,
+    portfolio: 'Retirement',
+    amount: 500,
+    depositFrequency: 'One-Time',
+  },
+  {
+    id: 2,
+    portfolio: 'Retirement',
+    amount: 100,
+    depositFrequency: 'Monthly',
+  },
+];
+
+const fundDeposits = [
+  {
+    id: 1,
+    amount: 10500,
+  },
+  {
+    id: 2,
+    amount: 100,
+  },
+];
+
+const expectedResult = {
+  totalDepositAmount: 10600,
+  allocation: ['High risk = $10000', 'Retirement = $600'],
+};
+
+describe('e2e', () => {
+  it('should handle depositing funds correctly', () => {
+    cy.visit('/deposit');
+
+    cy.findByRole('button', {
+      name: /deposit funds/i,
+    }).click();
+
+    cy.findByRole('button', {
+      name: /next/i,
+    }).should('be.disabled');
+
+    for (const data of depositPlans) {
+      cy.findByRole('textbox', {
+        name: /portfolio name/i,
+      }).type(data.portfolio);
+
+      cy.findByRole('textbox', {
+        name: /amount/i,
+      }).type(data.amount.toString());
+
+      cy.findByRole('radio', {
+        name: /one-time/i,
+      }).should('be.checked');
+
+      if (data.depositFrequency === 'Monthly') {
+        cy.findByRole('radio', {
+          name: /monthly/i,
+        }).click();
+
+        cy.findByRole('radio', {
+          name: /monthly/i,
+        }).should('be.checked');
+      }
+
+      cy.findByRole('button', {
+        name: /add/i,
+      }).click();
+
+      cy.wait(500);
+    }
+
+    cy.findByRole('button', {
+      name: /next/i,
+    }).click();
+
+    for (const data of fundDeposits) {
+      cy.findByRole('textbox', {
+        name: /amount/i,
+      }).type(data.amount.toString());
+
+      cy.findByRole('button', {
+        name: /add/i,
+      }).click();
+
+      cy.wait(500);
+    }
+
+    cy.findByRole('button', {
+      name: /next/i,
+    }).click();
+
+    cy.wait(1000);
+
+    cy.findByText(`$${expectedResult.totalDepositAmount.toString()}`).should(
+      'exist',
+    );
+
+    for (const data of expectedResult.allocation) {
+      cy.findByText(data).should('exist');
+    }
+  });
+});
+
+export {};
