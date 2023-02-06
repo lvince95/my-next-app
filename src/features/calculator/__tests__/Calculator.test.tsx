f18e24eb40e9e206190e7e9262a50315f2c2bb70
Vincent <lvince95@gmail.com>
2023-02-07 01:30:46 +0800

    :hammer: feat: improve calculator unit tests

--- src/features/calculator/__tests__/Calculator.test.tsx
+++ src/features/calculator/__tests__/Calculator.test.tsx
@@ -1,6 +1,6 @@
-import Calculator from '@/pages/calculator';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { CalculatorForm } from '../components/CalculatorForm';
 
 const testData = [
   { input: '1 + 1', expected: '2' },
@@ -16,35 +16,40 @@ const testData = [
   { input: '10 - ( 2 + 3 * ( 7 - 5 ) )', expected: '2' },
 ];
 
-describe.each(testData)(
-  'evaluate all expressions with the basic calculator',
-  ({ input, expected }) => {
-    test('should render an input, compute button, and clear button', async () => {
-      render(<Calculator />);
+const testDataWrongInput = ['1 + )', '1 + - -5', '1>3', '1+3', '11'];
 
-      expect(
-        screen.getByRole('heading', {
-          name: /calculator/i,
-        }),
-      ).toBeInTheDocument();
+describe.each(testData)(
+  'evaluate all valid expressions with the calculator',
+  ({ input, expected }) => {
+    test('should calculate result after submitting', async () => {
+      const user = userEvent.setup();
+
+      render(<CalculatorForm />);
 
       expect(
         screen.getByRole('textbox', { name: /arithmetic expression/i }),
       ).toBeInTheDocument();
 
+      await user.type(
+        screen.getByRole('textbox', { name: /arithmetic expression/i }),
+        input,
+      );
+
       expect(
         screen.getByRole('button', { name: /compute/i }),
       ).toBeInTheDocument();
 
-      expect(
-        screen.getByRole('button', { name: /clear/i }),
-      ).toBeInTheDocument();
+      await user.click(screen.getByRole('button', { name: /compute/i }));
+
+      await waitFor(() =>
+        expect(screen.queryByText(`Result: ${expected}`)).toBeInTheDocument(),
+      );
     });
 
-    test('should calculate after submitting', async () => {
+    test('should clear input and results after clicking on clear', async () => {
       const user = userEvent.setup();
 
-      render(<Calculator />);
+      render(<CalculatorForm />);
 
       await user.type(
         screen.getByRole('textbox', { name: /arithmetic expression/i }),
@@ -56,12 +61,14 @@ describe.each(testData)(
       await waitFor(() =>
         expect(screen.queryByText(`Result: ${expected}`)).toBeInTheDocument(),
       );
-    });
 
-    test('should clear input and results after clearing', async () => {
-      const user = userEvent.setup();
+      expect(screen.getByLabelText(/arithmetic expression/i)).toHaveValue(
+        input,
+      );
 
-      render(<Calculator />);
+      expect(
+        screen.getByRole('button', { name: /clear/i }),
+      ).toBeInTheDocument();
 
       await user.click(screen.getByRole('button', { name: /clear/i }));
 
@@ -70,6 +77,35 @@ describe.each(testData)(
           screen.queryByText(`Result: ${expected}`),
         ).not.toBeInTheDocument(),
       );
+
+      expect(screen.getByLabelText(/arithmetic expression/i)).toHaveValue('');
     });
   },
 );
+
+describe.each(testDataWrongInput)('check for input errors', (input) => {
+  test('should show error message after submitting', async () => {
+    const user = userEvent.setup();
+
+    render(<CalculatorForm />);
+
+    expect(
+      screen.getByRole('textbox', { name: /arithmetic expression/i }),
+    ).toBeInTheDocument();
+
+    await user.type(
+      screen.getByRole('textbox', { name: /arithmetic expression/i }),
+      input,
+    );
+
+    expect(
+      screen.getByRole('button', { name: /compute/i }),
+    ).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button', { name: /compute/i }));
+
+    await waitFor(() =>
+      expect(screen.queryByText(/invalid expression/i)).toBeInTheDocument(),
+    );
+  });
+});
